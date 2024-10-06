eae2ce9c91327d5a99c44741efc94756e7b08924
Raushan Kumar <raushan.development@gmail.com>
2024-10-06 15:04:09 +0530

    add: points about generators

--- generator.ts
+++ generator.ts
@@ -15,3 +15,10 @@ console.log(generator.next());
 console.log(generator.next());
 console.log(generator.next());
 
+/**
+ * Key points about generators:
+ * 1. They can be paused and resumed.
+ * 2. They automatically create an iterator.
+ * 3. They can yield multiple values.
+ * 4. They can be infinite.
+ */
